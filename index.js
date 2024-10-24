c46439c73790044a3de376a131e5b00d644ac100
barak <barak@UbuntuVM.myguest.virtualbox.org>
2024-10-24 13:22:38 +0300

    another change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World, text changed!')
+  res.send('Hello World, text changed again!')
 })
 
 app.listen(port, () => {

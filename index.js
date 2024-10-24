c693fd55a511d239a0d61185a61f0de60ee37f5a
barak <barak@UbuntuVM.myguest.virtualbox.org>
2024-10-24 15:18:25 +0300

    three times

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World, text changed twice!')
+  res.send('Hello World, text changed three times!')
 })
 
 app.listen(port, () => {
